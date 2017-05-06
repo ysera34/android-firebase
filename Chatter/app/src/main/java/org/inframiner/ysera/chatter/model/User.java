ac768f81c3974267e0d79e81af3b10329b373c7f
ysera34 <ycyoon34@gmail.com>
2017-05-06 13:51:27 +0900

    start chatter

--- Chatter/app/src/main/java/org/inframiner/ysera/chatter/model/User.java
+++
@@ -1,8 +0,0 @@
-package org.inframiner.ysera.chatter.model;
-
-/**
- * Created by yoon on 2017. 2. 18..
- */
-
-public class User {
-}
--- Chatter/app/src/main/java/org/inframiner/ysera/chatter/view/SignInActivity.java
+++ Chatter/app/src/main/java/org/inframiner/ysera/chatter/view/SignInActivity.java
@@ -1,23 +1,33 @@
 package org.inframiner.ysera.chatter.view;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import org.inframiner.ysera.chatter.R;
 
+/**
+ * Created by yoon on 2017. 5. 4..
+ */
+
 public class SignInActivity extends AppCompatActivity {
 
-
+    private Fragment mFragment;
+    private FragmentManager mFragmentManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.container_sign_in, SignInFragment.newInstance())
-                .commit();
+        mFragmentManager = getSupportFragmentManager();
+        mFragment = mFragmentManager.findFragmentById(R.id.container_sign_in);
+        if (mFragment == null) {
+            mFragmentManager.beginTransaction()
+                    .add(R.id.container_sign_in, SignInFragment.newInstance())
+                    .commit();
+        }
     }
-
-
 }
