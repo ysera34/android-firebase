6cd650eaaee49e0e53c4300f747af0d2e8aaeb41
ysera34 <ycyoon34@gmail.com>
2017-08-08 13:16:12 +0900

    notification sample

---
+++ Notifications/app/src/main/java/com/android/sample/notifications/app/Config.java
@@ -0,0 +1,8 @@
+package com.android.sample.notifications.app;
+
+/**
+ * Created by yoon on 2017. 8. 8..
+ */
+
+public class Config {
+}
---
+++ Notifications/app/src/main/java/com/android/sample/notifications/util/NotificationUtils.java
@@ -0,0 +1,8 @@
+package com.android.sample.notifications.util;
+
+/**
+ * Created by yoon on 2017. 8. 8..
+ */
+
+public class NotificationUtils {
+}
